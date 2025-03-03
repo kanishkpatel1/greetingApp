8ed7276c6bcd617f7951e2efb3915bacb929c267
Kanishk Patel <kanishkdeveshpatel@gmail.com>
2025-03-03 22:44:57 +0530

    uc 7 completed

---
+++ src/main/java/org/example/greetingapp/HelloController.java
@@ -0,0 +1,78 @@
+package org.example.greetingapp;
+
+import org.example.greetingapp.Model.Greeting;
+import org.example.greetingapp.repository.GreetingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    // UC1: Say "Hello World"
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+
+    // UC2: Say Hello with Name as Query Parameter
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+    // 3. Say Hello with Name in Path Variable
+
+    @GetMapping("/hello/param/{name}")
+    public String sayHelloPath(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+    // 4
+    @Autowired
+    private GreetingRepository greetingRepository;
+
+    @PostMapping("/greeting")
+    public String generateGreeting(@RequestBody Greeting greeting) {
+        String message;
+
+        if (greeting.getFirstName() != null && greeting.getLastName() != null) {
+            message = "Hello, " + greeting.getFirstName() + " " + greeting.getLastName() + "!";
+        } else if (greeting.getFirstName() != null) {
+            message = "Hello, " + greeting.getFirstName() + "!";
+        } else if (greeting.getLastName() != null) {
+            message = "Hello, " + greeting.getLastName() + "!";
+        } else {
+            message = "Hello, World!";
+        }
+
+        greeting.setMessage(message);
+        greetingRepository.save(greeting);
+        return message;
+    }
+
+    @GetMapping("/greeting/{id}")
+    public Greeting getGreeting(@PathVariable Long id) {
+        return greetingRepository.findById(id).orElse(null);
+    }
+
+// UC6 getting all the greeting
+    @GetMapping("/greeting")
+    public List<Greeting> getGreetings() {
+        return greetingRepository.findAll();
+    }
+
+    // Update an existing Greeting message
+    @PutMapping("/greeting/{id}")
+    public Greeting updateGreeting(@PathVariable Long id, @RequestBody Greeting updatedGreeting) {
+        return greetingRepository.findById(id)
+                .map(greeting -> {
+                    greeting.setMessage(updatedGreeting.getMessage());
+                    return greetingRepository.save(greeting);
+                })
+                .orElseThrow(() -> new RuntimeException("Greeting not found with id " + id));
+    }
+
+
+
+}
