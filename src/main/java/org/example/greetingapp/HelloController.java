733fdc1abceab98f1b55fb47b1b20c5bc7735b67
Kanishk Patel <kanishkdeveshpatel@gmail.com>
2025-03-03 22:32:34 +0530

    uc 2 completed

---
+++ src/main/java/org/example/greetingapp/HelloController.java
@@ -0,0 +1,21 @@
+package org.example.greetingapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // UC1: Say "Hello World"
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+    // UC2: Say Hello with Name as Query Parameter
+    @GetMapping("/hello/query")
+    public String sayHelloQuery(@RequestParam String name) {
+        return "Hello, " + name + "!";
+    }
+
+}
