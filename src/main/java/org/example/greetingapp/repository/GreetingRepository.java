fb4977190122604b6b89af5a1eb78fe1058f872c
Kanishk Patel <kanishkdeveshpatel@gmail.com>
2025-03-03 22:40:40 +0530

    uc 5 completed

---
+++ src/main/java/org/example/greetingapp/repository/GreetingRepository.java
@@ -0,0 +1,9 @@
+package org.example.greetingapp.repository;
+
+import org.example.greetingapp.Model.Greeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+}
